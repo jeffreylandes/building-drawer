4e3e2b87496a6207a53793bafb28e94fa460052c
Jeffrey <jeffrey@spacemaker.ai>
2021-11-07 14:50:28 -0500

    feat: change data to image draw

--- training/data.py
+++ training/data.py
@@ -1,66 +1,19 @@
 import math
-from dataclasses import dataclass
-from typing import Tuple
 
 import geopandas as gpd
-import matplotlib.pyplot as plt
 import numpy as np
-from PIL import Image
 from shapely.geometry import Polygon, Point, LineString
 from torch.utils.data import Dataset
-from tempfile import NamedTemporaryFile
 
-from training.data_utils import get_corner_points, BBox
-
-BUFFER = 0.002
-IMG_SHAPE = 800
-
-
-def plot_and_get_data(geometry: gpd.GeoSeries):
-    geometry.plot(figsize=(8, 8), markersize=1)
-    plt.axis("off")
-    with NamedTemporaryFile() as f:
-        plt.savefig(f)
-        plt.close("all")
-        img = Image.open(f)
-        data = np.array(img)
-        return data[:, :, 0].astype(float)
-
-
-def standardize_direction(direction: float):
-    return (direction - -180) / 360
-
-
-def plot_geometry(geometry: gpd.GeoSeries, corners: gpd.GeoSeries):
-    all_geometry = corners.append(geometry)
-    all_geometry_data = plot_and_get_data(all_geometry)
-    return all_geometry_data
-
-
-def get_keep_percentage():
-    keep_percentage = np.random.normal(loc=0.75, scale=0.4)
-    keep_percentage = max(keep_percentage, 0)
-    keep_percentage = min(keep_percentage, 1)
-    return keep_percentage
-
-
-def get_bbox_from_center_point(polygon: Polygon):
-    center_point = polygon.centroid.coords
-    center_lat, center_lon = list(center_point)[0]
-    lat_min, lat_max = center_lat - BUFFER, center_lat + BUFFER
-    lon_min, lon_max = center_lon - BUFFER, center_lon + BUFFER
-    return BBox(lat_min=lat_min, lon_min=lon_min, lat_max=lat_max, lon_max=lon_max)
-
-
-def get_final_bounding_box(
-    bounding_box: BBox, data_bounds: Tuple[float, float, float, float]
-):
-    data_lat_min, data_lon_min, data_lat_max, data_lon_max = data_bounds
-    lat_min = min(bounding_box.lat_min, data_lat_min)
-    lat_max = max(bounding_box.lat_max, data_lat_max)
-    lon_min = min(bounding_box.lon_min, data_lon_min)
-    lon_max = max(bounding_box.lon_max, data_lon_max)
-    return BBox(lat_min=lat_min, lon_min=lon_min, lat_max=lat_max, lon_max=lon_max)
+from training.data_utils import (
+    get_bbox_from_center_point,
+    get_final_bounding_box,
+    get_keep_percentage,
+    plot_geometry,
+    normalize_distance,
+    standardize_direction,
+    IMG_SHAPE,
+)
 
 
 class BuildingData(Dataset):
@@ -72,7 +25,7 @@ class BuildingData(Dataset):
         for geometry_index, geometry in enumerate(self.data.geometry):
             items = [
                 (geometry_index, coordinate_index)
-                for coordinate_index in range(len(geometry.exterior.coords))
+                for coordinate_index in range(len(geometry.exterior.coords) - 1)
             ]
             self.data_items.extend(items)
 
@@ -99,26 +52,24 @@ class BuildingData(Dataset):
 
         intersection_minus_building = kept_buildings[kept_buildings.id != building.id]
 
-        corner_points = get_corner_points(final_bounding_box)
-        corners = gpd.GeoSeries(corner_points)
-        corners_plot = plot_geometry(corners, corners)
         building_coordinates = list(building_polygon.exterior.coords)
         start_point = gpd.GeoSeries(Point(building_coordinates[coordinate_index]))
 
-        buildings = plot_geometry(intersection_minus_building.geometry, corners)
-        buildings = np.where(buildings != corners_plot, buildings, 0)
+        buildings_plot = plot_geometry(
+            intersection_minus_building.geometry, final_bounding_box, "polygon"
+        )
+        starting_point_plot = plot_geometry(start_point, final_bounding_box, "point")
         if coordinate_index > 0:
             first_lines = gpd.GeoSeries(
                 LineString(building_coordinates[: coordinate_index + 1])
             )
-            single_building = plot_geometry(first_lines, corners)
+            single_building_plot = plot_geometry(
+                first_lines, final_bounding_box, "line"
+            )
         else:
-            single_building = np.zeros_like(buildings)
-        single_building = np.where(single_building != corners_plot, single_building, 0)
-        starting_point = plot_geometry(start_point, corners)
-        starting_point = np.where(starting_point != corners_plot, starting_point, 0)
+            single_building_plot = np.zeros_like(buildings_plot)
 
-        if coordinate_index == len(building_coordinates) - 1:
+        if coordinate_index == len(building_coordinates) - 2:
             target = np.array([0, 0, 1])
             mask = np.array([0, 0, 1])
         else:
@@ -126,17 +77,18 @@ class BuildingData(Dataset):
                 building_coordinates[coordinate_index],
                 building_coordinates[coordinate_index + 1],
             )
+            distance_normalized = normalize_distance(distance)
             x_src, y_src = building_coordinates[coordinate_index]
             x_dst, y_dst = building_coordinates[coordinate_index + 1]
             direction = np.arctan2(y_dst - y_src, x_dst - x_src) * 180 / np.pi
             standardized_direction = standardize_direction(direction)
-            target = np.array([distance, standardized_direction, 0])
+            target = np.array([distance_normalized, standardized_direction, 0])
             mask = np.array([1, 1, 0])
 
         site = np.zeros((3, IMG_SHAPE, IMG_SHAPE))
-        site[0] = buildings
-        site[1] = single_building
-        site[2] = starting_point
+        site[0] = buildings_plot
+        site[1] = single_building_plot
+        site[2] = starting_point_plot
 
         data_item = {
             "site": site.astype(np.float32),
--- training/data_utils.py
+++ training/data_utils.py
@@ -1,7 +1,15 @@
 from dataclasses import dataclass
-from typing import List
+from typing import List, Tuple
 
-from shapely.geometry import Point
+import geopandas as gpd
+import numpy as np
+from PIL import Image, ImageDraw
+from shapely.geometry import Point, Polygon
+
+BUFFER = 0.001
+IMG_SHAPE = 500
+MEAN_DISTANCE = 0.0001166
+STD_DISTANCE = 0.00013627
 
 
 @dataclass(frozen=True)
@@ -20,3 +28,69 @@ def get_corner_points(bounding_box: BBox) -> List[Point]:
         Point(bounding_box.lat_max, bounding_box.lon_min),
     ]
     return corner_points
+
+
+def get_final_bounding_box(
+    bounding_box: BBox, data_bounds: Tuple[float, float, float, float]
+):
+    data_lat_min, data_lon_min, data_lat_max, data_lon_max = data_bounds
+    lat_min = min(bounding_box.lat_min, data_lat_min)
+    lat_max = max(bounding_box.lat_max, data_lat_max)
+    lon_min = min(bounding_box.lon_min, data_lon_min)
+    lon_max = max(bounding_box.lon_max, data_lon_max)
+    return BBox(lat_min=lat_min, lon_min=lon_min, lat_max=lat_max, lon_max=lon_max)
+
+
+def get_keep_percentage():
+    keep_percentage = np.random.normal(loc=0.75, scale=0.4)
+    keep_percentage = max(keep_percentage, 0)
+    keep_percentage = min(keep_percentage, 1)
+    return keep_percentage
+
+
+def validate_geometry_type(geometry_type: str):
+    assert geometry_type in [
+        "polygon",
+        "point",
+        "line",
+    ], f"Geometry type {geometry_type} is not supported. Must be one of: polygon, point, line"
+
+
+def plot_geometry(geometry: gpd.GeoSeries, bbox: BBox, geometry_type: str):
+    validate_geometry_type(geometry_type)
+    img = Image.new("L", (IMG_SHAPE, IMG_SHAPE), 255)
+    draw = ImageDraw.Draw(img)
+    for building in geometry.geometry:
+        if geometry_type == "point" or geometry_type == "line":
+            x, y = building.coords.xy
+        else:
+            x, y = building.exterior.coords.xy
+        x = np.array(x)
+        y = np.array(y)
+        x_transformed = (x - bbox.lat_min) / (bbox.lat_max - bbox.lat_min) * IMG_SHAPE
+        y_transformed = (y - bbox.lon_min) / (bbox.lon_max - bbox.lon_min) * IMG_SHAPE
+        transformed_coordinates = np.array(list(zip(x_transformed, y_transformed)))
+        transformed_coordinates = [tuple(c) for c in transformed_coordinates]
+        if geometry_type == "point":
+            draw.point(transformed_coordinates, fill=0)
+        elif geometry_type == "line":
+            draw.line(transformed_coordinates, fill=0)
+        else:
+            draw.polygon(transformed_coordinates, fill=0, outline=0)
+    return np.array(img) / 255
+
+
+def standardize_direction(direction: float):
+    return (direction - -180) / 360
+
+
+def normalize_distance(distance: float):
+    return (distance - MEAN_DISTANCE) / STD_DISTANCE
+
+
+def get_bbox_from_center_point(polygon: Polygon):
+    center_point = polygon.centroid.coords
+    center_lat, center_lon = list(center_point)[0]
+    lat_min, lat_max = center_lat - BUFFER, center_lat + BUFFER
+    lon_min, lon_max = center_lon - BUFFER, center_lon + BUFFER
+    return BBox(lat_min=lat_min, lon_min=lon_min, lat_max=lat_max, lon_max=lon_max)
